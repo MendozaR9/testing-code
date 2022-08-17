fac1b305c9203a118c271315880403ea68e6a144
Ryan Mendoza <ryanmendoza.00@gmail.com>
2022-08-17 15:14:01 -0500

    convert num to string and string to num

--- Java/Abstract/AnimalStuff.java
+++ Java/Abstract/AnimalStuff.java
@@ -1,6 +1,7 @@
 package Java.Abstract;
 
 public interface AnimalStuff {
-    //all methods are assume as abstract
+    //all methods are assumed as abstract and all field are set as static and final
+
     public void poop();
 }
---
+++ js/covertNumberToString.js
@@ -0,0 +1,9 @@
+function numToString (num){
+    console.log(num.toString());
+
+}
+function stringToNum(string){
+    console.log(parseInt(string));
+}
+numToString(25)
+stringToNum("25")
