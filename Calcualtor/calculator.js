539fffb93635c6f6b93f237ad7c5d0e5f12823d4
Ryan Mendoza <ryanmendoza.00@gmail.com>
2022-08-28 14:44:49 -0500

    create add button and equal button

--- Calcualtor/calculator.js
+++ Calcualtor/calculator.js
@@ -1,8 +1,37 @@
 var num;
+var num2 = 1
+var math = false
+var equation;
+
 $(document).ready(function (){
-    $(".btn").click(function (){
-        num = $(this).data('num')
-        $("#result").html(num)
-        console.log(num)
+    $(".equation").click(function (){
+        math = true;
+        equation =$(this).data('equation')
+        console.log(equation)
     })
+
+
+    $(".btn").click(function (){
+        $(this).css("background-color", "red")
+        if (math===false){
+            num = $(this).data('num')
+            console.log("num 1 = "+num)
+            $("#result").html(num)
+        }else {
+            num2 = $(this).data('num')
+            console.log("num 2 = "+num2)
+            $("#result").html(num2)
+        }
+    })
+
+    $("#equal").click(function (){
+        let result;
+        if (equation =="add"){
+            result= num+ num2;
+            $("#result").html(result)
+        }
+
+    })
+
+
 })
